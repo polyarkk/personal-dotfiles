d1d2ef16323a1901170d60f40ff04ebc9757e493
锦欢 <1781176460@qq.com>
2024-02-02 16:13:08 +0800

    Add files via upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,40 @@
+﻿function Write-BranchName {
+    try {
+        $branch = git rev-parse --abbrev-ref HEAD
+
+        if ($branch -eq "HEAD") {
+            $branch = git rev-parse --short HEAD
+            Write-Host "($branch) " -ForegroundColor "red" -NoNewLine
+        }
+        else {
+            Write-Host "($branch) " -ForegroundColor "cyan" -NoNewLine
+        }
+    } catch {
+        Write-Host "(no branches yet) " -ForegroundColor "yellow" -NoNewLine
+    }
+}
+
+function prompt {
+    $statusIndicator = $(if ($?) { " " } else { " x_x " })
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    $isAdmin = $($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+    $location = $(Get-Location).ToString().Replace($home, "~")
+    $computerNameColor = $(if ($isAdmin) { "red" } else { "green" })
+    $time = Get-Date -Format "HH:mm"
+    $userPrompt = " "
+
+    Write-Host "[" -NoNewLine
+    Write-Host -ForegroundColor yellow $time -NoNewLine
+    Write-Host "]" -NoNewLine
+    Write-Host -ForegroundColor red $statusIndicator -NoNewLine
+    Write-Host -ForegroundColor $computerNameColor $($env:computername.ToLower()) -NoNewLine
+    Write-Host -ForegroundColor blue " $location " -NoNewLine
+    
+    if (Test-Path .git) {
+        Write-BranchName
+    }
+
+    Write-Host -ForegroundColor blue $(if ($isAdmin) { "#" } else { "$" }) -NoNewLine
+
+    return $userPrompt   
+}
